7fa65e12492833d331292c9e6b3b1f2aa48645de
R4ID3NZ4P <iamxtremesifat@gmail.com>
2023-11-08 20:06:01 +0600

    Error page added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,14 +1,19 @@
 import { Helmet } from "react-helmet";
 import titles from "../../titles/titles";
+import errorImage from "../../assets/error404.png"
+import { Link } from "react-router-dom";
 
 
 const Error = () => {
     return (
-        <div>
+        <div className="bg-[#D5CBE5] h-screen">
             <Helmet>
                 <title>{titles.error}</title>
             </Helmet>
-            error 404
+            <div className="h-full flex flex-col justify-center items-center">
+                <img src={errorImage} className="lg:w-1/2" />
+                <Link to={"/"} className="btn btn-primary">Go Back To Home Page</Link>
+            </div>
         </div>
     );
 };
