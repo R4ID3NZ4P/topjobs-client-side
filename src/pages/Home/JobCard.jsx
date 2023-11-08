8b279f060b060024ad531ccdedb3331151bd84f4
R4ID3NZ4P <iamxtremesifat@gmail.com>
2023-11-08 19:04:29 +0600

    framer-motion animation used in Home page (jobs section)

--- src/pages/Home/JobCard.jsx
+++ src/pages/Home/JobCard.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion"
 
 
-const JobCard = ({ data }) => {
+const JobCard = ({ data, isVisible }) => {
 
     const {
         _id,
@@ -17,20 +18,27 @@ const JobCard = ({ data }) => {
     } = data;
 
     return (
-        <div className="card bg-base-100 shadow-xl image-full">
-            <figure><img src={image} alt={title} /></figure>
-            <div className="card-body gap-1">
-                <h2 className="card-title mb-6 text-warning">{title}</h2>
-                <p><span className="font-semibold text-warning">Posted by </span>{username}</p>
-                <p><span className="font-semibold text-warning">Salary Range </span>${salary}</p>
-                <p><span className="font-semibold text-warning">Posted on </span>{postdate}</p>
-                <p><span className="font-semibold text-warning">Deadline </span>{deadline}</p>
-                <p><span className="font-semibold text-warning">Applicants </span>{applicants}</p>
-                <div className="card-actions justify-end">
-                <Link to={`/job/${_id}`} className="btn btn-warning btn-outline rounded-none text-white">View Details</Link>
+        <AnimatePresence>
+                <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                >
+                <div className="card bg-base-100 shadow-xl image-full">
+                    <figure><img src={image} alt={title} /></figure>
+                    <div className="card-body gap-1">
+                        <h2 className="card-title mb-6 text-warning">{title}</h2>
+                        <p><span className="font-semibold text-warning">Posted by </span>{username}</p>
+                        <p><span className="font-semibold text-warning">Salary Range </span>${salary}</p>
+                        <p><span className="font-semibold text-warning">Posted on </span>{postdate}</p>
+                        <p><span className="font-semibold text-warning">Deadline </span>{deadline}</p>
+                        <p><span className="font-semibold text-warning">Applicants </span>{applicants}</p>
+                        <div className="card-actions justify-end">
+                        <Link to={`/job/${_id}`} className="btn btn-warning btn-outline rounded-none text-white">View Details</Link>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </motion.div>
+        </AnimatePresence>
     );
 };
 
